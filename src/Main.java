b313d009d7391e244dc9021bbe0404488a6ffbf3
Griotold <gotjd9773@naver.com>
2023-09-07 12:26:33 +0900

    침투 - 13565

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 
 public class Main {
+    // MAX map visited N M dy dx flag
 
     public static void main(String[] args) {
+        // 0 . 입력 및 초기화
+
+        // 1. map 정보 반영 str.charAt(idx)
+
+
+        // 2. dfs 수행
+
+
+        // 3. 출력
 
     }
 }
