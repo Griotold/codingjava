8ed977a00aff50890d5f5d5031fc797216f5f64c
Griotold <gotjd9773@naver.com>
2023-09-09 17:13:15 +0900

    프로그래머스 - 타겟 넘버 DFS

--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,31 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String[][] clothes) {
-        // 1. 옷을 종류 별로 구분한다.
-        HashMap<String, Integer> map = new HashMap<>();
-        for (String[] clothe : clothes) {
-            String type = clothe[1];
-            map.put(type, map.getOrDefault(type, 0) + 1);
+    int[] numbers;
+    int target;
+    int answer;
+
+    void dfs(int idx, int sum) {
+        // 1. 탈출 조건
+        if (idx == numbers.length) {
+            if (sum == target) answer++;
+            return;
         }
 
-        // 2. 입지 않는 경우를 추가해서 모든 조합을 계산한다.
-        int answer = 1;
-        Iterator<Integer> it = map.values().iterator();
-        while(it.hasNext())
-            answer *= it.next().intValue() + 1;
+        // 2. 수행 동작
+        dfs(idx + 1, sum + numbers[idx]);
+        dfs(idx + 1, sum - numbers[idx]);
+    }
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        this.numbers = numbers;
+        this.target = target;
 
-        // 3. 아무 종류의 옷도 입지 않는 경우 제외한다.
-        return answer - 1;
+        dfs(0, 0);
+
+        return answer;
     }
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
-        String[][] clothes = { {"yellowhat", "headgear"},
-                                {"bluesunglasses", "eyewear"},
-                                {"green_turban", "headgear"}};
-        System.out.println(sol.solution(clothes));
     }
 }
