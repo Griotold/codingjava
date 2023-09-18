791d30d9ae8f04fca2edccf41ae766ccc862c4e3
Griotold <gotjd9773@naver.com>
2023-09-18 13:24:07 +0900

    김태원 Java - C++강의에 있는 문제 Java로 풀기 - 1. 1부터 N까지 수 중 M의 배수합 출력

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,32 @@
-import kim.string.문자찾기_1;
-
-import java.util.*;
 import java.io.*;
-/**
- * 한개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에
- * 몇 개 존재하는지 알아내는 프로그램을 작성해라
- * 대소문자 구분 안함. 문자열의 길이는 100이하
- * Computercooler
- * c
- * */
+import java.util.*;
 public class Main {
-
-    public int solution(String str, char c) {
+    /**
+     * 자연수 N이 입력되면 1부터 N까지 수 중
+     * M의 배수들의 합을 출력하는 프로그램
+     * 입력 : 15 3
+     * 출력 : 45
+     * */
+    public int solution(int N, int M) {
         int answer = 0;
-
-        // 1. 모두 소문자로 변환
-        String lowerCase = str.toLowerCase();
-
-        // 2. 해쉬 맵
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < str.length(); i++) {
-            char key = lowerCase.charAt(i);
-            map.put(key, map.getOrDefault(key, 0) + 1);
+        // 1. N까지 반복문을 돌다가 배수라면 answer에 더한다
+        for(int i = 1; i <= N; i++){
+            if (i % M == 0) answer += i;
         }
 
-        // 3. 키 값을 넣었을 때 몇 개 있는지 리턴
-        answer = map.get(c);
+        // 2. answer 출력
+
         return answer;
     }
 
     public static void main(String[] args) throws IOException {
-        Main main = new Main();
-        문자찾기_1 문자찾기_1 = new 문자찾기_1();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String str = br.readLine();
-        char c = br.readLine().charAt(0);
-        System.out.println(main.solution(str, c));
-        System.out.println(문자찾기_1.solution(str, c));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        Main main = new Main();
+        System.out.println(main.solution(N, M));
         br.close();
         bw.close();
     }
