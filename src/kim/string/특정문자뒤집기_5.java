9c95647a6205ff8fddb4a15f8b236740ba654d64
Griotold <gotjd9773@naver.com>
2023-09-18 17:35:47 +0900

    김태원 Java - String 5번문제 - 특정문자뒤집기 - 추가

---
+++ src/kim/string/특정문자뒤집기_5.java
@@ -0,0 +1,33 @@
+package kim.string;
+
+public class 특정문자뒤집기_5 {
+    /**
+     * 5. 특정 문자 뒤집기
+     * 영어는 뒤집고 특수문자는 자기 자리 그대로
+     * a#b!GE*T@S -> S#T!EGb@a
+     * */
+    public String solution(String str) {
+        char[] answer = str.toCharArray();
+        int lt = 0;
+        int rt = str.length() - 1;
+        while(lt < rt){
+            if (!Character.isAlphabetic(answer[lt])){
+                // 왼쪽 애가 특수 문자일 때
+                lt++;
+            } else if(!Character.isAlphabetic(answer[rt])) {
+                // 오른쪽 애가 특수 문자일 때
+                rt--;
+            } else {
+                // 둘다 알파벳일 때
+                char temp = answer[lt];
+                answer[lt] = answer[rt];
+                answer[rt] = temp;
+                lt++;
+                rt--;
+            }
+        }
+//        String answerStr = answer.toString();
+        String answerStr = String.valueOf(answer);
+        return new String(answer);
+    }
+}
