f31bfa4991b5f46f3e1402557ad1c1a82d6c3aea
Griotold <gotjd9773@naver.com>
2023-09-18 13:16:49 +0900

    김태원 Java - 1. 문자 찾기

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,43 @@
+import kim.string.문자찾기_1;
+
 import java.util.*;
 import java.io.*;
+/**
+ * 한개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에
+ * 몇 개 존재하는지 알아내는 프로그램을 작성해라
+ * 대소문자 구분 안함. 문자열의 길이는 100이하
+ * Computercooler
+ * c
+ * */
 public class Main {
-    static final int MAX = 25 + 10;
-    static boolean[][] map;
-    static boolean[][] visited;
-    static int N;
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
-    static int countPerDanzi;
 
-    static void dfs(int y, int x) {
-        visited[y][x] = true;
-        countPerDanzi++;
+    public int solution(String str, char c) {
+        int answer = 0;
 
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (map[ny][nx] && !visited[ny][nx])
-                dfs(ny, nx);
+        // 1. 모두 소문자로 변환
+        String lowerCase = str.toLowerCase();
+
+        // 2. 해쉬 맵
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char key = lowerCase.charAt(i);
+            map.put(key, map.getOrDefault(key, 0) + 1);
         }
 
+        // 3. 키 값을 넣었을 때 몇 개 있는지 리턴
+        answer = map.get(c);
+        return answer;
     }
+
     public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        문자찾기_1 문자찾기_1 = new 문자찾기_1();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        N = Integer.parseInt(br.readLine());
-        map = new boolean[MAX][MAX];
-        visited = new boolean[MAX][MAX];
-        for(int i = 1; i <= N; i++) {
-            String str = br.readLine();
-            for(int j = 1; j <= N; j++) {
-                map[i][j] = str.charAt(j - 1) == '1';
-            }
-        }
-
-        ArrayList<Integer> countList = new ArrayList<>();
-        for(int i = 1; i <= N; i++) {
-            for(int j = 1; j <= N; j++) {
-                if (map[i][j] && !visited[i][j]){
-                    countPerDanzi = 0;
-                    dfs(i, j);
-                    countList.add(countPerDanzi);
-                }
-            }
-        }
-        bw.write(String.valueOf(countList.size()));
-        bw.newLine();
-        Collections.sort(countList);
-        for (int count : countList) {
-            bw.write(String.valueOf(count));
-            bw.newLine();
-        }
+        String str = br.readLine();
+        char c = br.readLine().charAt(0);
+        System.out.println(main.solution(str, c));
+        System.out.println(문자찾기_1.solution(str, c));
         br.close();
         bw.close();
     }
---
+++ src/kim/string/문자찾기_1.java
@@ -0,0 +1,30 @@
+package kim.string;
+
+import java.util.Locale;
+
+/**
+ * 한개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에
+ * 몇 개 존재하는지 알아내는 프로그램을 작성해라
+ * 대소문자 구분 안함. 문자열의 길이는 100이하
+ * Computercooler
+ * c
+ * */
+public class 문자찾기_1 {
+
+    public int solution(String str, char c) {
+        int answer = 0;
+
+        // 1. 소문자로 변환
+        str = str.toLowerCase();
+        c = Character.toLowerCase(c);
+
+        // 2. 반복문 -> c와 일치한다면 answer를 증가
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) == c) answer++;
+        }
+
+        // 3. return 증가
+
+        return answer;
+    }
+}
