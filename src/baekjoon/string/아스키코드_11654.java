ea153639fde502ff2a8183ca264e94edb0119672
Griotold <gotjd9773@naver.com>
2023-09-19 16:03:22 +0900

    백준 - String - 아스키코드 11654

--- src/baekjoon/string/아스키코드_11654.java
+++ src/baekjoon/string/아스키코드_11654.java
@@ -1,11 +1,10 @@
+package baekjoon.string;
+
 import java.io.*;
-import java.util.*;
-public class Main {
-    /**
-     * 아스키 코드
-     * 말 그대로 아스키 코드 값 출력
-     * */
-    public static void main(String[] args) throws IOException {
+
+public class 아스키코드_11654 {
+
+    public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String s = br.readLine();
