a37342582719180b44313c8af7c382d7f1d2db09
Griotold <gotjd9773@naver.com>
2023-09-27 21:00:49 +0900

    Kim - 2. Array - 9. 격자판 최대합

--- src/kim/array/격자판최대합_9.java
+++ src/kim/array/격자판최대합_9.java
@@ -1,13 +1,8 @@
-import java.io.*;
-import java.util.*;
-public class Main {
+package kim.array;
+
+public class 격자판최대합_9 {
     /**
-     * 5
-     * 10 13 10 12 15
-     * 12 39 30 23 11
-     * 11 25 50 53 15
-     * 19 27 29 37 27
-     * 19 13 30 13 19
+     * 내 풀이
      * */
     static int[][] map;
     public int solution(int N, int[][] arr) {
@@ -41,18 +36,30 @@ public class Main {
         if(tmp > answer) answer = tmp;
         return answer;
     }
-    public static void main(String[] args) throws IOException {
-        Main T = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        for(int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+    /**
+     * 강사님 풀이
+     * */
+    public int solution2(int n, int[][] arr) {
+        int answer = Integer.MIN_VALUE;
+        int sum1, sum2;
+        for(int i = 0; i < n; i++) {
+            sum1 = sum2 = 0;
+            for(int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
             }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
         }
-        System.out.println(T.solution(N, map));
-        br.close();
+        sum1 = sum2 = 0;
+        for(int i = 0; i < n; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][n-i-1];
+        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+        return answer;
     }
+
+
 }
