f884742eff894d088a47d7c5459bb8b1eb84ac7d
Griotold <gotjd9773@naver.com>
2023-10-06 17:08:05 +0900

    Kim - 5. Stack & Queue - 5. 쇠막대기

--- src/kim/ch5_stack_queue/쇠막대기_5.java
+++ src/kim/ch5_stack_queue/쇠막대기_5.java
@@ -1,7 +1,8 @@
-import java.io.*;
-import java.util.*;
-public class Main {
-    // ()(((()())(())()))(()) -> 17
+package kim.ch5_stack_queue;
+
+import java.util.Stack;
+
+public class 쇠막대기_5 {
     public int solution(String str) {
         int answer = 0;
         Stack<Character> stack = new Stack<>();
@@ -19,12 +20,5 @@ public class Main {
             }
         }
         return answer;
-    }
-    public static void main(String[] args) throws IOException {
-        Main T = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        System.out.println(T.solution(str));
-        br.close();
     }
 }
