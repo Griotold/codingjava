49ac94029308080697abb4ddc19fe6cb59a2d66e
Griotold <gotjd9773@naver.com>
2023-10-10 18:48:20 +0900

    Kim - 6. sort & binary search & decision algorithm - 8. 이분 검색

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,36 @@
 import java.io.*;
 import java.util.*;
 
-class Point implements Comparable<Point> {
-    public int x, y;
 
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public int compareTo(Point o) {
-        if(this.x == o.x) return this.y - o.y;
-        return this.x - o.x;
-    }
-}
 public class Main {
-    // 5
-    //2 7
-    //1 3
-    //1 2
-    //2 5
-    //3 6
-//    public int[][] sol(int N, int[][] arr) {
-//        return arr;
-//    }
+    // 8 32
+    // 23 87 65 12 57 32 99 81 -> 3
+    public int sol(int N, int M, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 0, rt = N - 1;
+        while(lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if(arr[mid] == M) {
+                answer = mid + 1;
+                break;
+            }
+            else if (arr[mid] > M) rt = mid - 1;
+            else lt = mid + 1;
+        }
+        return answer;
+    }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        ArrayList<Point> list = new ArrayList<>();
-        for(int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            list.add(new Point(x, y));
-        }
-        Collections.sort(list);
-        for (Point p : list) System.out.println(p.x + " " +p.y);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        System.out.println(T.sol(N, M, arr));
         br.close();
     }
 }
