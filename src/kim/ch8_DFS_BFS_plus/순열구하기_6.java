519c05c3bf8fe5c2e274eda4fe85f33b9b5130f7
Griotold <gotjd9773@naver.com>
2023-10-18 18:55:45 +0900

    Kim - 8. DFS & BFS - 6. 순열 구하기

--- src/kim/ch8_DFS_BFS_plus/순열구하기_6.java
+++ src/kim/ch8_DFS_BFS_plus/순열구하기_6.java
@@ -1,9 +1,11 @@
-import java.io.*;
-import java.util.*;
+package kim.ch8_DFS_BFS_plus;
 
-public class Main {
-    // 3 2
-    // 3 6 9
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 순열구하기_6 {
     static int N, M;
     static int[] arr;
     static int[] answer;
@@ -25,9 +27,8 @@ public class Main {
         }
     }
 
-
     public static void main(String[] args) throws IOException {
-        Main T = new Main();
+        순열구하기_6 T = new 순열구하기_6();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
