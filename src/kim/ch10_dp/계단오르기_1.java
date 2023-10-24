0b1df7a76968aac036f4b98d1c7a6704da43c202
Griotold <gotjd9773@naver.com>
2023-10-24 17:15:11 +0900

    Kim - 10. dynamic programming - 1. 계단 오르기

--- src/kim/ch10_dp/계단오르기_1.java
+++ src/kim/ch10_dp/계단오르기_1.java
@@ -1,12 +1,15 @@
+package kim.ch10_dp;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
-    // 4 --> 5
-    // 7 --> 21
+public class 계단오르기_1 {
     static int N;
     static int[] dp;
+    /**
+     * 내 풀이 - 재귀 활용
+     */
     public int DFS(int N) {
         if(dp[N] != 0) return dp[N];
         if(N == 1) return 1;
@@ -16,8 +19,20 @@ public class Main {
         }
     }
 
+    /**
+     * 강사님 풀이 - 반복문 활용
+     */
+    public int solByTeacher(int n) {
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) throws IOException {
-        Main T = new Main();
+        계단오르기_1 T = new 계단오르기_1();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         dp = new int[N + 1];
