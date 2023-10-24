fe5332556e0feccc2ece8b5b6c57b41cebb9d45d
Griotold <gotjd9773@naver.com>
2023-10-24 17:21:51 +0900

    Kim - 10. dynamic programming - 2. 돌다리 건너기

--- src/kim/ch10_dp/돌다리건너기_2.java
+++ src/kim/ch10_dp/돌다리건너기_2.java
@@ -1,9 +1,10 @@
+package kim.ch10_dp;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
-    // 7 -> 34
+public class 돌다리건너기_2 {
     static int N;
     static int[] dp;
     public int DFS(int N) {
@@ -11,16 +12,25 @@ public class Main {
         if(N == 1) return 1;
         if(N == 2) return 2;
         else {
-            return dp[N] = DFS(N-1) + DFS(N-2);
+            return dp[N] = DFS(N - 1) + DFS(N - 2);
         }
     }
 
+    public int solByTeacher(int n) {
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i = 2];
+        }
+        return dp[n + 1];
+    }
+
     public static void main(String[] args) throws IOException {
-        Main T = new Main();
+        돌다리건너기_2 T = new 돌다리건너기_2();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         dp = new int[N + 2];
-        System.out.println(T.DFS(N+1));
+        System.out.println(T.DFS(N + 1));
         br.close();
     }
 }
