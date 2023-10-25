94ea77ce0d72657923dab990e6a060fa8f0d23b1
Griotold <gotjd9773@naver.com>
2023-10-25 18:50:51 +0900

    Kim - 10. dynamic programming - 5. 동전교환 (냅색 알고리즘)

--- src/kim/ch10_dp/동전교환_냅색_5.java
+++ src/kim/ch10_dp/동전교환_냅색_5.java
@@ -1,6 +1,12 @@
-import java.io.*;
-import java.util.*;
-public class Main {
+package kim.ch10_dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 동전교환_냅색_5 {
     static int N, M;
     static int[] dp;
     public int sol(int[] coin) {
@@ -15,7 +21,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        Main T = new Main();
+        동전교환_냅색_5 T = new 동전교환_냅색_5();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
