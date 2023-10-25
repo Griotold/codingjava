64e8ca9ceaf3c2aac3ca306bf4734947af54e794
Griotold <gotjd9773@naver.com>
2023-10-25 18:58:56 +0900

    Kim - 10. dynamic programming - 6. 최대 점수 구하기

--- src/kim/ch10_dp/최대점수구하기_6.java
+++ src/kim/ch10_dp/최대점수구하기_6.java
@@ -1,8 +1,13 @@
-import java.io.*;
-import java.util.*;
-public class Main {
+package kim.ch10_dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 최대점수구하기_6 {
+
     public static void main(String[] args) throws IOException {
-        Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
