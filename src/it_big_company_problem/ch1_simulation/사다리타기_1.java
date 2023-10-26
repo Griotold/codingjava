b9da283fda1480b2b82971426edae38ce80a5d9e
Griotold <gotjd9773@naver.com>
2023-10-26 17:14:39 +0900

    자바 코딩테스트 - it 대기업 유제 ch1. 시뮬레이션 & 구현 - 1. 사다리 타기

--- src/it_big_company_problem/ch1_simulation/사다리타기_1.java
+++ src/it_big_company_problem/ch1_simulation/사다리타기_1.java
@@ -1,6 +1,11 @@
-import java.util.*;
+package it_big_company_problem.ch1_simulation;
 
-class Solution {
+import java.util.Arrays;
+
+public class 사다리타기_1 {
+    /**
+     * 내 풀이
+     * */
     public char[] solution(int n, int[][] ladder){
         char[] answer = new char[n];
         // 1. answer에 알파벳 초기화
@@ -19,8 +24,28 @@ class Solution {
         return answer;
     }
 
+    /**
+     * 강사님 풀이 - 동일
+     * 코딩 스타일의 차이
+     */
+    public char[] solByTeacher(int n, int[][] ladder) {
+        char[] answer = new char[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = (char)(i + 65);
+        }
+
+        for (int[] line : ladder) {
+            for (int x : line) {
+                char tmp = answer[x];
+                answer[x] = answer[x - 1];
+                answer[x - 1] = tmp;
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args){
-        Solution T = new Solution();
+        사다리타기_1 T = new 사다리타기_1();
         System.out.println(Arrays.toString(T.solution(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
         System.out.println(Arrays.toString(T.solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
         System.out.println(Arrays.toString(T.solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
