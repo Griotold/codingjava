df0e2adce1c1ad81acdc1b29d1260a80b28d5e6e
Griotold <gotjd9773@naver.com>
2023-10-27 17:50:01 +0900

    자바 코딩테스트 - it 대기업 유제 ch1. 시뮬레이션 & 구현 - 2. 청소

--- src/it_big_company_problem/ch1_simulation/청소_2.java
+++ src/it_big_company_problem/ch1_simulation/청소_2.java
@@ -1,7 +1,13 @@
-import java.util.*;
+package it_big_company_problem.ch1_simulation;
 
-class Solution {
-
+import java.util.Arrays;
+/**
+ * 해낼 수 있다고 생각하면 해낼 수 있게 된다.
+ * */
+public class 청소_2 {
+    /**
+     * 내 풀이
+     * */
     public int[] solution(int[][] board, int k){
         int[] answer = new int[2]; // 2, 2
         int[] dy = {0, 1, 0, -1};
@@ -28,8 +34,33 @@ class Solution {
         return answer;
     }
 
+    /**
+     * 강사님 풀이 - 내 풀이랑 동일, 코딩 스타일 차이
+     */
+    public int[] solByTeacher(int[][] board, int k) {
+        int[] answer = new int[2];
+        int n = board.length;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+        int x = 0, y = 0, d = 1, count = 0;
+        while (count < k) {
+            count++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n || board[nx][ny] == 1) {
+                d = (d + 1) % 4;
+                continue;
+            }
+            x = nx;
+            y = ny;
+        }
+        answer[0] = x;
+        answer[1] = y;
+        return answer;
+    }
+
     public static void main(String[] args){
-        Solution T = new Solution();
+        청소_2 T = new 청소_2();
         int[][] arr1 = {{0, 0, 0, 0, 0},
                 {0, 1, 1, 0, 0},
                 {0, 0, 0, 0, 0},
