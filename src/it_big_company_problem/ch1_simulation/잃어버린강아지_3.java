4e4438171898a52fd371bfbe26d360e90d018701
Griotold <gotjd9773@naver.com>
2023-10-29 16:21:19 +0900

    자바 코딩테스트 - it 대기업 유제 ch1. 시뮬레이션 & 구현 - 3. 잃어버린 강아지

--- src/it_big_company_problem/ch1_simulation/잃어버린강아지_3.java
+++ src/it_big_company_problem/ch1_simulation/잃어버린강아지_3.java
@@ -1,7 +1,12 @@
-import java.util.*;
-
-class Solution {
-
+package it_big_company_problem.ch1_simulation;
+/**
+ * 청소_2 문제랑 비슷
+ * 두 개체가 이동한다는 것만 다름
+ * */
+public class 잃어버린강아지_3 {
+    /**
+     * 내 풀이
+     * */
     public int solution(int[][] board){
         int answer = 0, count = 0, max = 10000;
         int y = 0, x = 0, dogY = 0, dogX = 0;
@@ -50,8 +55,60 @@ class Solution {
         return answer;
     }
 
+    /**
+     * 강사님 풀이
+     */
+    public int solByTeacher(int[][] board) {
+        int n = board.length;
+        int[] dy = {-1, 0, 1, 0};
+        int[] dx = {0, 1, 0, -1};
+        int y1 = 0, x1 = 0, y2 = 0, x2 = 0;
+        // 1. 초기화
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 2) {
+                    y1 = i;
+                    x1 = j;
+                }
+                if (board[i][j] == 3) {
+                    y2 = i;
+                    x2 = j;
+                }
+            }
+        }
+        // 2. 시뮬레이션
+        int d1 = 0, d2 = 0, count = 0;
+        while (count < 10000) {
+            count++;
+            int ny1 = y1 + dy[d1];
+            int nx1 = x1 + dx[d1];
+            int ny2 = y2 + dy[d2];
+            int nx2 = x2 + dx[d2];
+            boolean flag1 = true, flag2 = true;
+            if (nx1 < 0 || nx1 >= n || ny1 < 0 || ny1 >= n || board[ny1][nx1] == 1) {
+                d1 = (d1 + 1) % 4;
+                flag1 = false;
+            }
+            if (nx2< 0 || nx2 >= n || ny2 < 0 || ny2 >= n || board[ny2][nx2] == 1) {
+                d2 = (d2 + 1) % 4;
+                flag2 = false;
+            }
+            if (flag1 == true) {
+                y1 = ny1;
+                x1 = nx1;
+            }
+            if (flag2 == true) {
+                y2 = ny2;
+                x2 = nx2;
+            }
+            if(x1 == x2 && y1 == y2) break;
+        }
+        if(count >= 10000) return 0;
+        return count;
+    }
+
     public static void main(String[] args){
-        Solution T = new Solution();
+        잃어버린강아지_3 T = new 잃어버린강아지_3();
         int[][] arr1 = {{0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
                 {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
                 {0, 0, 0, 1, 0, 0, 0, 1, 0, 0},
@@ -62,7 +119,7 @@ class Solution {
                 {0, 0, 0, 0, 0, 3, 0, 0, 0, 1},
                 {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
                 {0, 1, 0, 1, 0, 0, 0, 0, 0, 0}};
-        System.out.println(T.solution(arr1));
+        System.out.println(T.solByTeacher(arr1));
         int[][] arr2 = {{1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
                 {0, 0, 1, 1, 0, 0, 0, 1, 0, 0},
@@ -73,6 +130,6 @@ class Solution {
                 {0, 0, 1, 0, 0, 0, 0, 0, 2, 1},
                 {0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
                 {0, 1, 0, 1, 0, 0, 0, 0, 0, 3}};
-        System.out.println(T.solution(arr2));
+        System.out.println(T.solByTeacher(arr2));
     }
 }
