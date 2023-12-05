07232247dcf254122056dd1b7f6dc33d7fb6f4da
Griotold <gotjd9773@naver.com>
2023-12-05 18:47:12 +0900

    자바 코딩테스트 - it 대기업 유제 ch2. 해싱 - 4. 음수가 있는 부분수열

--- src/it_big_company_problem/ch2_hash/음수가있는부분수열_4.java
+++ src/it_big_company_problem/ch2_hash/음수가있는부분수열_4.java
@@ -1,6 +1,29 @@
-import java.util.*;
+package it_big_company_problem.ch2_hash;
 
-class Solution {
+import java.util.HashMap;
+
+public class 음수가있는부분수열_4 {
+    /**
+     * 정답
+     */
+    public int solByTeacher(int[] nums, int m) {
+        int answer = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        map.put(0, 1);
+        for (int x : nums) {
+            sum += x;
+            if (map.containsKey(sum - m)) {
+                answer += map.get(sum - m);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return answer;
+    }
+
+    /**
+     * 내 풀이
+     * */
     public int solution(int[] nums, int m){
         int answer = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -19,7 +42,7 @@ class Solution {
     }
 
     public static void main(String[] args){
-        Solution T = new Solution();
+        음수가있는부분수열_4 T = new 음수가있는부분수열_4();
         System.out.println(T.solution(new int[]{2, 2, 3, -1, -1, -1, 3, 1, 1}, 5));
         System.out.println(T.solution(new int[]{1, 2, 3, -3, 1, 2, 2, -3}, 5));
         System.out.println(T.solution(new int[]{1, 2, 3, -3, 1, 2}, 3));
