ba8deed91489a8167feb50ce94c7b1686379eb78
Griotold <gotjd9773@naver.com>
2023-12-05 18:00:17 +0900

    자바 코딩테스트 - it 대기업 유제 ch2. 해싱 - 3. 서로 다른 빈도수 만들기

--- src/it_big_company_problem/ch2_hash/서로다른빈도수만들기_3.java
+++ src/it_big_company_problem/ch2_hash/서로다른빈도수만들기_3.java
@@ -1,6 +1,38 @@
-import java.util.*;
+package it_big_company_problem.ch2_hash;
 
-class Solution {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class 서로다른빈도수만들기_3 {
+
+    /**
+     * 정답
+     */
+    public int solByTeacher(String s) {
+        int answer = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashSet<Integer> ch = new HashSet<>();
+
+        // 1. 해싱
+        for (char x : s.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        // 2. 유니크한 빈도수 집합이 되도록 빈도수를 제거
+        for (char key : map.keySet()) {
+            while (ch.contains(map.get(key))) {
+                answer++;
+                map.put(key, map.get(key) - 1);
+            }
+            if (map.get(key) == 0) continue;
+            ch.add(map.get(key));
+        }
+        return answer;
+    }
+    /**
+     * 내 풀이
+     * */
     public int solution(String s){
         int answer = 0;
         HashMap<Character, Integer> map = new HashMap<>();
@@ -24,12 +56,11 @@ class Solution {
     }
 
     public static void main(String[] args){
-        Solution T = new Solution();
+        서로다른빈도수만들기_3 T = new 서로다른빈도수만들기_3();
         System.out.println(T.solution("aaabbbcc"));
         System.out.println(T.solution("aaabbc"));
         System.out.println(T.solution("aebbbbc"));
         System.out.println(T.solution("aaabbbcccde"));
         System.out.println(T.solution("aaabbbcccdddeeeeeff"));
     }
-
 }
