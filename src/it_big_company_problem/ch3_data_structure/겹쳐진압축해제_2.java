1463ab92c6982d4a13d38abb3b715bd97de87fd6
Griotold <gotjd9773@naver.com>
2023-12-08 18:19:19 +0900

    자바 코딩테스트 - it 대기업 유제 ch3. 자료구조 활용 - 2. 겹쳐진 압축 해제

--- src/it_big_company_problem/ch3_data_structure/겹쳐진압축해제_2.java
+++ src/it_big_company_problem/ch3_data_structure/겹쳐진압축해제_2.java
@@ -1,40 +1,40 @@
-import java.util.*;
+package it_big_company_problem.ch3_data_structure;
 
-class Solution {
+import java.util.Stack;
+
+public class 겹쳐진압축해제_2 {
+
     public String solution(String s){
         String answer = "";
-        Stack<String> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == ')') {
+        Stack<String> st = new Stack<>();
+        for (Character x : s.toCharArray()) {
+            if (x == ')') {
                 String tmp = "";
-                while (!stack.empty()) {
-                    String e = stack.pop();
-                    if (e.equals("(")) {
+                while(!st.empty()){
+                    String c = st.pop();
+                    if (c.equals("(")) {
                         String num = "";
-                        while (!stack.empty() && Character.isDigit(stack.peek().charAt(0))) {
-                            num = stack.pop() + num;
+                        while (!st.empty() && Character.isDigit(st.peek().charAt(0))) {
+                            num = st.pop() + num;
                         }
                         String res = "";
                         int cnt = 0;
                         if (num.equals("")) cnt = 1;
                         else cnt = Integer.parseInt(num);
                         for (int i = 0; i < cnt; i++) res += tmp;
-                        stack.push(res);
+                        st.push(res);
                         break;
                     }
-                    tmp = e + tmp;
+                    tmp = c + tmp;
                 }
-            } else {
-                stack.push(String.valueOf(ch));
-            }
+            } else st.push(String.valueOf(x));
         }
-        for (String x : stack) answer += x;
-
+        for (String x : st) answer += x;
         return answer;
     }
 
     public static void main(String[] args){
-        Solution T = new Solution();
+        겹쳐진압축해제_2 T = new 겹쳐진압축해제_2();
         System.out.println(T.solution("3(a2(b))ef"));
         System.out.println(T.solution("2(ab)k3(bc)"));
         System.out.println(T.solution("2(ab3((cd)))"));
