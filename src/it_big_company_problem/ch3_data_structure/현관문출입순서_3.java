5a67840a2f0f3d725fe67750cafb5da1277fdbb6
Griotold <gotjd9773@naver.com>
2023-12-11 18:26:58 +0900

    자바 코딩테스트 - it 대기업 유제 ch3. 자료구조 활용 - 3. 현관문 출입 순서 - Queue 자료구조

--- src/it_big_company_problem/ch3_data_structure/현관문출입순서_3.java
+++ src/it_big_company_problem/ch3_data_structure/현관문출입순서_3.java
@@ -1,15 +1,18 @@
-import java.util.*;
+package it_big_company_problem.ch3_data_structure;
 
-class Solution {
-    // new int[]{0, 1, 1, 1, 2, 3, 8, 8}, new int[]{1, 0, 0, 1, 0, 0, 1, 0}
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 현관문출입순서_3 {
     public int[] solution(int[] arrival, int[] state){
-        int n = arrival.length;
-        int[] answer = new int[n];
-        int prev = 1;
         Queue<Integer> enter = new LinkedList<>();
         Queue<Integer> exit = new LinkedList<>();
-
+        int n = arrival.length;
+        int prev = 1;
+        int[] answer = new int[n];
         for (int t = 0, i = 0, cnt = 0; ; t++) {
+            // 두 큐가 비어 있으나 아직 도착할 사원이 남아 있을 때
             if (enter.isEmpty() && exit.isEmpty() && i < n) {
                 if (t < arrival[i]) {
                     t = arrival[i];
@@ -17,12 +20,14 @@ class Solution {
                 }
             }
 
+            // 큐에 넣는 작업
             while (i < n && arrival[i] <= t) {
-                if (state[i] == 0) enter.offer(i);
+                if(state[i] == 0) enter.offer(i);
                 else exit.offer(i);
                 i++;
             }
 
+            // 조건에 따라 answer값 세팅
             if (prev == 1) {
                 if (!exit.isEmpty()) {
                     answer[exit.poll()] = t;
@@ -31,7 +36,7 @@ class Solution {
                     answer[enter.poll()] = t;
                     prev = 0;
                 }
-            } else {
+            } else if (prev == 0) {
                 if (!enter.isEmpty()) {
                     answer[enter.poll()] = t;
                     prev = 0;
@@ -43,12 +48,11 @@ class Solution {
             cnt++;
             if (cnt == n) break;
         }
-
         return answer;
     }
 
     public static void main(String[] args){
-        Solution T = new Solution();
+        현관문출입순서_3 T = new 현관문출입순서_3();
         System.out.println(Arrays.toString(T.solution(new int[]{0, 1, 1, 1, 2, 3, 8, 8}, new int[]{1, 0, 0, 1, 0, 0, 1, 0})));
         System.out.println(Arrays.toString(T.solution(new int[]{3, 3, 4, 5, 5, 5}, new int[]{1, 0, 1, 0, 1, 0})));
         System.out.println(Arrays.toString(T.solution(new int[]{2, 2, 2, 3, 4, 8, 8, 9, 10, 10}, new int[]{1, 0, 0, 0, 1, 1, 0, 1, 1, 0})));
